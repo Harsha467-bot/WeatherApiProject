acb94a2da611316429c269a5bcd241ea0455a160
HarshaNagarajAbbyy <harsha.nagaraju@abbyy.com>
2025-02-03 12:15:46 +0530

    commiting the project

---
+++ WEBAPIPROJECT/Healthcheck/WeatherApiHealthCheck.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace WEBAPIPROJECT.Healthcheck
+{
+    public class WeatherApiHealthCheck:IHealthCheck
+    {
+        public readonly HttpClient _httpClient;
+        public readonly string webapiurl = "http://localhost:5054/WeatherForecast";
+        public WeatherApiHealthCheck(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+
+            var response = await _httpClient.GetAsync(webapiurl, cancellationToken);
+             
+            if(response.IsSuccessStatusCode)
+            {
+                return HealthCheckResult.Healthy("API IS HEALTHY");
+
+            }
+            else
+            {
+                return HealthCheckResult.Unhealthy("API IS UNHEALTHY");
+            }
+        }
+    }
+}
---
+++ WEBAPIPROJECT/Program.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddHttpClient<HealthCheckResult>();   
+//adding health check to an api
+builder.Services.AddHealthChecks()
+    .AddCheck("API", () => HealthCheckResult.Healthy("API is healthy"))
+    .AddCheck("External API", () => HealthCheckResult.Healthy("External API is reachable"), tags: new[] { "external" });
+
+
+
+
+var app = builder.Build();
+app.MapGet("/", () => Results.Redirect("/swagger"));
+
+app.MapHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = async (context, report) =>
+    {
+        context.Response.ContentType = "application/json";
+        var result = new
+        {
+            status = report.Status.ToString(),
+            checks = report.Entries.Select(e => new
+            {
+                name = e.Key,
+                status = e.Value.Status.ToString(),
+                description = e.Value.Description
+            })
+        };
+
+        await context.Response.WriteAsJsonAsync(result);
+    }
+});
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    options.SwaggerEndpoint("/openapi/v1.json" ,"demoapi")
+    );
+
+
+}
+
+//app.MapHealthChecks("/health");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
